cef2eb7727d86974aea96a7cbb63f9578ca9919b
Jérémie Bonal <jeremie.bonal@gmail.com>
2019-10-23 13:13:22 +0100

    I know it's not ideal but it's temporary.

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,13 @@ Rails.application.routes.draw do
 
   resources :pages, param: :position, only: [:show]
 
-  resources :stories, param: :slug, only: [:index, :show] do
-    resources :pages, params: :position, controller: 'story_pages', only: [:show]
+  # FIXME: Develop proper arc and story management features so we can uncomment
+  #        this.
+  # resources :stories, param: :slug, only: [:index, :show] do
+  #   resources :pages, params: :position, controller: 'story_pages', only: [:show]
 
-    resources :arcs, param: :slug, only: [:show] do
-      resources :pages, params: :position, controller: 'arc_pages', only: [:show]
-    end
-  end
+  #   resources :arcs, param: :slug, only: [:show] do
+  #     resources :pages, params: :position, controller: 'arc_pages', only: [:show]
+  #   end
+  # end
 end
