6978762590f5365bbaa926b89cefac090db9cdd0
Jérémie Bonal <jeremie.bonal@gmail.com>
2019-10-31 13:06:32 +0100

    Abstracted content_for :header into #header helpher.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,16 @@ module ApplicationHelper
     classes << html_options[:class].to_s
     content_tag :i, nil, class: classes, **html_options.except(:class)
   end
+
+  def header(&block)
+    if block_given?
+      content_for :header, &block
+    else
+      if content_for :header
+        content_for :header
+      else
+        content_tag :h2, "Sans titre ?"
+      end
+    end
+  end
 end
